94e65d57b24e44feede43f81ef3798198ed71d5e
SimonOConnor28 <simonoconnor28@gmail.com>
2022-06-22 17:26:30 -0400

    fixed merge bug in code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <Header />
       <Monday />
       <Tuesday />
@@ -22,22 +21,6 @@ function App() {
       <Saturday />
       <Sunday />
       <Footer />
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React  
-        </a>
-      </header>
->>>>>>> production
     </div>
   );
 }
